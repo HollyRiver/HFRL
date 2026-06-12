7431145bf617d104146971e82f3003812992ec44
HollyRiver <hcssk2800@gmail.com>
2026-06-12 07:15:33 +0000

    .

--- SurvLLM/rlhf.sh
+++ SurvLLM/rlhf.sh
@@ -38,9 +38,10 @@ nohup python preference_AIF.py --model_name="Qwen/Qwen3-30B-A3B"\
                                --discharge_name="data/dpo_prompt_data.csv" &
 
 ## SFT에서 온전한 모델을 픽스하고, 데이터셋이 준비되었을 때
-nohup python csv_to_json_dataset.py --target="data/data_sample_max_for_DPO_20260205.csv"\
+nohup python csv_to_json_dataset.py --target="data/HF_Llama_Preference_v1.1.2.csv"\
                                     --encoding="utf-8"\
-                                    --system="data/system_prompt.txt" &&
+                                    --system="data/system_prompt.txt"\
+                                    --name_tag="_HF" &&
 
 nohup python DPO.py --config config/DPO_config_v1.1.0.yaml > logs/dpo_log_v1.1.0.txt &&
 ## Multi-GPU를 사용할 경우: Adapter Twice Load로 일단 되긴 하는데, 메모리 더 많이 먹음...
