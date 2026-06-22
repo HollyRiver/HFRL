bd96fdc1ce4dd2c24dd2daa9b28b7cd2934691fc
HollyRiver <hcssk2800@gmail.com>
2026-06-22 01:45:55 +0000

    .

--- SurvLLM/ppo.sh
+++ SurvLLM/ppo.sh
@@ -8,13 +8,13 @@ nohup python csv_to_json_dataset.py --target="data/ppo_dataset.csv"\
                                     --ppo=true &
 
 ## 단순 입력 프롬프트만 존재하는 데이터 사용
-nohup python PPO.py --config config/PPO_config_v1.1.2.9H.yaml > logs/ppo_log_v1.1.2.9H.txt &
+nohup python PPO.py --config config/PPO_config_v1.1.2.11H.yaml > logs/ppo_log_v1.1.2.11H.txt &
 
 ## PPO에서 온전한 모델을 픽스하고, 양자화된 base model이 따로 저장되었으며, 추론에 사용할 프롬프트가 준비되었을 때
 nohup python vllm_inference.py --base_model_path="base_model/Llama-3.1-8B-Instruct-nf4"\
-                               --adapter_path="adapter/PPO-Llama-v1.1.2.9H"\
+                               --adapter_path="adapter/PPO-Llama-v1.1.2.11H"\
                                --inference_data="data/inference_data.json"\
-                               --output_dir="data/inference_PPO_RLHF_v1.1.2.9.csv"\
+                               --output_dir="inference/inference_PPO_RLHF_v1.1.2.11.csv"\
                                --sampling=True\
                                --repetition_penalty=1.0\
                                --gpu_memory_util=0.9\
